6b8f30c70c23129dd3f300d3fe650b1f35ec0e76
Zafar Saleem <zafarsaleem3@gmail.com>
2023-09-28 20:04:26 +0500

    feat: em to other units converter

--- packages/unit-converter/components/em/emtopc/emtopc.tsx
+++ packages/unit-converter/components/em/emtopc/emtopc.tsx
@@ -3,17 +3,23 @@ import Flex from "@utils/flex"
 import Separator from "@utils/separator"
 import React from "react";
 import { Calculate } from "@/calculate-btn";
+import { useConverter } from "../../hooks/useConverter";
 
 export const EMToPC = ({ ...props }) => {
-  const [userFirstValue, updateUserFirstValue] = React.useState<number>(0);
-  const [result, updateResult] = React.useState<number>();
+  const { 
+    updateUserFirstValue,
+    result,
+    calculate,
+  } = useConverter("emtopc");
+  // const [userFirstValue, updateUserFirstValue] = React.useState<number>(0);
+  // const [result, updateResult] = React.useState<number>();
 
-  const calculate = React.useCallback(() => {
-    if (userFirstValue) {
-      // const total = userFirstValue * 0.4167;
-      // updateResult(total);
-    }
-  }, [userFirstValue]);
+  // const calculate = React.useCallback(() => {
+  //   if (userFirstValue) {
+  //     // const total = userFirstValue * 0.4167;
+  //     // updateResult(total);
+  //   }
+  // }, [userFirstValue]);
   
   return (
     <>
